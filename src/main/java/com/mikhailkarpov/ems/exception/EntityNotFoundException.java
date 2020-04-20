e360965cc4f4da1110ef0af6d3ad85bfa6cbca5c
Mikhail Karpov <mikhailkarpov@hotmail.com>
2020-04-21 01:04:00 +0200

    Exception handling

--- src/main/java/com/mikhailkarpov/ems/exception/EntityNotFoundException.java
+++ src/main/java/com/mikhailkarpov/ems/exception/EntityNotFoundException.java
@@ -1,5 +1,9 @@
 package com.mikhailkarpov.ems.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such employee")
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException(String message) {
