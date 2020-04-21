21ab26c93ecddb33c489ce91fa882e12927393c0
Mikhail Karpov <mikhailkarpov@hotmail.com>
2020-04-21 23:55:16 +0200

    Config datasource

--- src/main/resources/database-create.sql
+++
@@ -1,5 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS employeedb;
-
-CREATE USER IF NOT EXISTS 'java'@'localhost' IDENTIFIED BY 'javapassword';
-
-GRANT SELECT, INSERT, UPDATE ON employeedb.* TO 'java'@'localhost';
